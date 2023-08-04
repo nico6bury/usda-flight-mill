79d29efef474b1d688d5315d5ca60bd721fa7296
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2023-08-04 13:57:51 -0500

    Updated program version.

--- src/flightmill/ProcessDataLoggerFile.java
+++ src/flightmill/ProcessDataLoggerFile.java
@@ -52,7 +52,7 @@ public class ProcessDataLoggerFile {
     // header info
     private static String TITLE = "USDA-ARS Manhattan, KS\tMar/2023\tSixbury/Rust/Brabec";
     private static String PROGRAM_NAME = "Flight Mill Compression";
-    private static String VERSION = "v.2.23";
+    private static String VERSION = "v1.0.1";
 
     private static AppInterface gui;
 
