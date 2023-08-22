2bbf4cf020a5feabb7a8aba5a56a928073537847
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2023-08-22 12:09:48 -0500

    Updated defaults.

--- src/flightmill/dataStorageStructs/InputCommandLine.java
+++ src/flightmill/dataStorageStructs/InputCommandLine.java
@@ -4,12 +4,12 @@ package flightmill.dataStorageStructs;
 public class InputCommandLine {
     public String inputFileName = "default_input.csv";
     public String outputFileName = "default_input.out";
-    public int skipLines = 3;    // header lines at top of data logger file
+    public int skipLines = 4;    // header lines at top of data logger file
     public double timeInterval = 0.01;  // time between data logger readings
     public int numberOfChannelsUsed = 0;    // self explanitory
     public boolean zipFileFlg = false;              // zip input file
     public boolean dataTimeFlg = true;  // add date time to each output line
-    public boolean peakWidthFlg = false; // add date time to each output line
+    public boolean peakWidthFlg = true; // add date time to each output line
     public boolean doubleColumnFlg = true; // add second peakwidth and elapsedTime
     public double threshold = 1.5;
     public double thresh_seconds_fast = 0.300;
