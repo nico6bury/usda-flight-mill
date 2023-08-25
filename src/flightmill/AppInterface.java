f7207e74ba65c1f0648fcbf4961c0dcd32274f88
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2023-08-25 11:01:26 -0500

    Made header not-editable.

--- src/flightmill/AppInterface.java
+++ src/flightmill/AppInterface.java
@@ -85,6 +85,7 @@ public class AppInterface extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jTextArea1.setEditable(false);
         jTextArea1.setColumns(20);
         jTextArea1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jTextArea1.setRows(5);
